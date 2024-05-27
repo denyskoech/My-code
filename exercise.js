c68dbcc7419566349cf2d9c189d0e6775c4c4dae
denyskoech <dennis96ist@gmail.com>
2024-05-27 17:44:00 +0300

    JS files added and some code

---
+++ exercise.js
@@ -0,0 +1,9 @@
+let country = Kenya;
+let continent = Africa;
+let population = '50,000,000';
+console.log(country, continent, population);
+
+
+//Data types practise 
+const isIsland = 'Kenya';
+const language = ;
---
+++ follow.js
@@ -0,0 +1,47 @@
+/*let js = "amazing";
+
+let myCountry = "Kenya";
+let myContinent = "Africa";
+let myCountryPopulation = 50000000;
+
+console.log(myCountry);
+console.log(myContinent);
+console.log(myCountryPopulation);
+*/
+
+/*
+//math operators
+const now = 2047; // good idea to declare 2047 as a variable to reuse
+const ageUser = now - 1991; //the equal sign is an assignment operator assigning agerUser to now - 1991
+const ageClient = now - 2024;
+console.log(ageUser, ageClient);
+
+console.log(ageClient * 2, ageUser / 10, 2 ** 3); //2**3 means 2 to the powe of 3 which is 2 x 2 x 2
+
+const firstName = "Dennis";
+const lastName = "Koech";
+console.log(firstName + "" + lastName); //we add an empty string to separate the variable but there is a better way of doing that using template strings
+
+//assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; //= 100
+x++; // x = x + 1 = 101
+x--; // 99
+console.log(x);
+
+// comparison operators to produce boolean values
+console.log(ageClient > ageUser); //<,>, <=, >=
+console.log(ageClient >= 18); //check if user is 18 years old, these results should be stored in variables
+
+const isFullAge = ageClient >= 18; //here we have stored the results to a variable
+
+console.log(now - 1991 > now - 2024); //JS will do the math for both ages first before it can compare. Order precedence like BODMAS, find in MDN
+
+let x, y; //it is possible to declare two variables like this which are undefined
+x = y = 25 - 10 - 5; //operator precedence, math operators first and then assignment operator
+console.log(x, y); //= 10, 10 because the assignment operator works from right to left, assigning 10 to Y and since x is = to y then x also becomes 10
+
+const averageAge = ageClient + ageUser / 2; //this will produce 55.5 which does not make sense because of operatore precedence, to fix, place brackets which will be calculated first and the divided like this (ageClient + ageUser) / 2
+console.log(averageAge);
+*/
