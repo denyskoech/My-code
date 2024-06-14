7a89a7622317aba3c56e61c0597063a6f00cc642
denyskoech <dennis96ist@gmail.com>
2024-06-14 09:16:40 +0300

    Dice game V1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -8,26 +8,74 @@
 
 // document.querySelector('.guess').value = 23;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1; //*20 gives us a range of 1- 19.0000 +1 makes it 20, math.trunc removes the decimals
+let secretNumber = Math.trunc(Math.random() * 20) + 1; //*20 gives us a range of 1- 19.0000 +1 makes it 20, math.trunc removes the decimals
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber; //set it to the variable number
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'Are you sure?';
-  } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'You are awesome!';
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Ai buda, mbali!';
-    score--;
-    document.querySelector('.score').textContent = score;
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Acha za ovyo!';
-    score--;
-    document.querySelector('.score').textContent = score;
+  }
+
+  //when the player wins
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Wazi buda!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347'; //css in JS needs to be in camel case and always specify with a string
+
+    document.querySelector('.number').style.width = '30rem'; //30rem has to be a string, in quotes and this is inline css
+
+    document.querySelector('.number').textContent = secretNumber; //set it to the variable number
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+
+  //When the guess is higher than expected number
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Ai buda, mbali!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+
+    //when the game is lost
+    else {
+      document.querySelector('.message').textContent = 'Iza buda!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  //when the guess is lower than the expected number
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Acha za ovyo!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+
+    //when the game is lost
+    else {
+      document.querySelector('.message').textContent = 'Iza buda!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 }); // note that we did not call the function but the JS engine will call it as soon as the event happens
+
+document.querySelector('.again').addEventListener('click', function () {
+  //this is an anonymous handler function
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Tuanze tena';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
