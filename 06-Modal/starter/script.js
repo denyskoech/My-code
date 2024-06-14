c9019d5c64e8eceec8db8c3d6a689f6f5784dfa1
denyskoech <dennis96ist@gmail.com>
2024-06-14 10:18:30 +0300

    Some project definations

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overLay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('show-modal'); //use queryselectorALL to select all the properties in the class
+
+for (i = 0; i < btnsOpenModal; i++) {
+  //we don't really need {}
+}
