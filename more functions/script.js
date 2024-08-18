80d493d9b36fce6364b8866e53c43d486019ab41
denyskoech <dennis96ist@gmail.com>
2024-08-18 22:23:39 +0300

    primitive vs reference type in functions

--- more functions/script.js
+++ more functions/script.js
@@ -24,4 +24,24 @@ const createBooking = function (
 
 //Passing arguments working in functions: primitives and reference
 
-const flight
+const flight = "LH234";
+const jonas = {
+  name: "Jonas",
+  passport: 2345467465,
+};
+
+const checkIn = function (flightNum1, passenger) {
+  // flightNum1 is like saying flightNum1 = flight, flightNum1 is a copy of flight var
+  flightNum1 = "LH999"; //attempt to change flight number but it is a completly different variable
+  passenger.name = "Mr. " + passenger.name; //ref type, changing the copy changes the original as well, stored in the heap
+
+  if (passenger.passport === 2345467465) {
+    alert("Checked in");
+  } else {
+    alert("Wrong passport");
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight); //returns original flight number
+console.log(jonas); //returns object plus MR included
